2ad05331b3ead5e33127092826a4fa5418595111
miqo221 <miqonazarian@gmail.com>
2021-11-15 19:30:13 +0400

    added new files

---
+++ index.js
@@ -0,0 +1,9 @@
+var a = 5;
+var b = 5;
+var square = (a+b)*(a+b);
+var bStr = "b = " + b;
+var squareStr = "(a + b)^2 = " + square; 
+
+console.log("a = " + a);
+console.log(bStr);
+console.log(squareStr);
